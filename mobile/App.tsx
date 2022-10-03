2c53c3fd4346366831eb194f65319ee8bcf62a75
asalmeidarj <asalmeidarj@gmail.com>
2022-10-03 20:06:14 -0300

    importe do tema no arquivo principal

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,8 +1,42 @@
 import { Background } from 'components/Background';
 import { StatusBar } from 'react-native';
-import { Text } from 'react-native';
+import { Home } from 'screens/Home';
+
+import AppLoading from 'expo-app-loading';
+
+import {
+  useFonts,
+  Roboto_100Thin,
+  Roboto_100Thin_Italic,
+  Roboto_300Light,
+  Roboto_300Light_Italic,
+  Roboto_400Regular,
+  Roboto_400Regular_Italic,
+  Roboto_500Medium,
+  Roboto_500Medium_Italic,
+  Roboto_700Bold,
+  Roboto_700Bold_Italic,
+  Roboto_900Black,
+  Roboto_900Black_Italic,
+} from '@expo-google-fonts/roboto';
 
 export default function App() {
+
+  let [fontsLoaded] = useFonts({
+    Roboto_100Thin,
+    Roboto_100Thin_Italic,
+    Roboto_300Light,
+    Roboto_300Light_Italic,
+    Roboto_400Regular,
+    Roboto_400Regular_Italic,
+    Roboto_500Medium,
+    Roboto_500Medium_Italic,
+    Roboto_700Bold,
+    Roboto_700Bold_Italic,
+    Roboto_900Black,
+    Roboto_900Black_Italic,
+  });
+  
   return (
     <Background>
       <StatusBar 
@@ -10,7 +44,7 @@ export default function App() {
         backgroundColor="transparent"
         translucent
       />
-      
+      {fontsLoaded ? <Home /> : <AppLoading />}
     </Background>
   );
 }
