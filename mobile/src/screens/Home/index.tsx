47cb9eda021c7651c402c838035dfdbec7947947
asalmeidarj <asalmeidarj@gmail.com>
2022-10-03 20:03:25 -0300

    Inserção do cabeçalho

--- mobile/src/screens/Home/index.tsx
+++ mobile/src/screens/Home/index.tsx
@@ -1,10 +1,12 @@
 import { Background } from 'components/Background';
+import { Header } from 'components/Header';
 import { Text, View } from 'react-native';
 import { styles } from './styles';
 
 export function Home() {
   return (
     <Background>
+        <Header />
         <View style={styles.container}>
             <Text style={styles.text}> Home </Text>
         </View>
